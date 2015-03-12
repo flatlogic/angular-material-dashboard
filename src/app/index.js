c3a395cdc25f4e083ddb596303a4318ad9d5e4d2
micrum <rumiancau@gmail.com>
2015-03-12 15:11:04 +0300

    generate application using yeoman

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('angularMaterialAdmin', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ui.router', 'ngMaterial'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,64 @@
+'use strict';
+
+angular.module('angularMaterialAdmin')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      {
+        'title': 'AngularJS',
+        'url': 'https://angularjs.org/',
+        'description': 'HTML enhanced for web apps!',
+        'logo': 'angular.png'
+      },
+      {
+        'title': 'BrowserSync',
+        'url': 'http://browsersync.io/',
+        'description': 'Time-saving synchronised browser testing.',
+        'logo': 'browsersync.png'
+      },
+      {
+        'title': 'GulpJS',
+        'url': 'http://gulpjs.com/',
+        'description': 'The streaming build system.',
+        'logo': 'gulp.png'
+      },
+      {
+        'title': 'Jasmine',
+        'url': 'http://jasmine.github.io/',
+        'description': 'Behavior-Driven JavaScript.',
+        'logo': 'jasmine.png'
+      },
+      {
+        'title': 'Karma',
+        'url': 'http://karma-runner.github.io/',
+        'description': 'Spectacular Test Runner for JavaScript.',
+        'logo': 'karma.png'
+      },
+      {
+        'title': 'Protractor',
+        'url': 'https://github.com/angular/protractor',
+        'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
+        'logo': 'protractor.png'
+      },
+      {
+        'title': 'jQuery',
+        'url': 'http://jquery.com/',
+        'description': 'jQuery is a fast, small, and feature-rich JavaScript library.',
+        'logo': 'jquery.jpg'
+      },
+      {
+        'title': 'Angular Material Design',
+        'url': 'https://material.angularjs.org/#/',
+        'description': 'The Angular reference implementation of the Google\'s Material Design specification.',
+        'logo': 'angular-material.png'
+      },
+      {
+        'title': 'Sass (Ruby)',
+        'url': 'http://sass-lang.com/',
+        'description': 'Original Syntactically Awesome StyleSheets implemented in Ruby',
+        'logo': 'ruby-sass.png'
+      }
+    ];
+    angular.forEach($scope.awesomeThings, function(awesomeThing) {
+      awesomeThing.rank = Math.random();
+    });
+  });
