bf01a4e5b17c5ec8f79450ca5bb40dad1975d8f3
micrum <rumiancau@gmail.com>
2015-07-17 14:52:35 +0300

    add visitors chart

--- src/app/controllers/VisitorsController.js
+++ src/app/controllers/VisitorsController.js
@@ -1,29 +1,28 @@
 (function () {
     angular
         .module('app')
-        .controller('UsageController', [
-            UsageController
+        .controller('VisitorsController', [
+            VisitorsController
         ]);
 
-    function UsageController() {
+    function VisitorsController() {
         var vm = this;
 
         // TODO: move data to the service
-        vm.ramChartData = [{key: 'Memory', y: 768660}, { key: 'Cache', y: 367404}, {key: 'Swap', y: 41924 }];
-        vm.storageChartData = [{key: 'System', y: 126560}, {key: 'Other', y: 224365 }];
+        vm.visitorsChartData = [ {key: 'Mobile', y: 5264}, { key: 'Desktop', y: 3872} ];
 
         vm.chartOptions = {
             chart: {
                 type: 'pieChart',
-                height: 130,
+                height: 210,
                 donut: true,
                 x: function (d) { return d.key; },
                 y: function (d) { return d.y; },
                 valueFormat: (d3.format(".0f")),
-                color: ['rgb(0, 150, 136)', '#E75753', 'rgb(235, 235, 235)'],
+                color: ['rgb(0, 150, 136)', '#E75753'],
                 showLabels: false,
                 showLegend: false,
-                title: '83%'
+                title: 'Over 9K'
             }
         };
     }
