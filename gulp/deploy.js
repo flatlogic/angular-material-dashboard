d7cd0c1508d7e59846d2086b42d7fd38f9066a60
micrum <rumiancau@gmail.com>
2016-01-15 22:22:15 +0300

    Update 2016-01-15T19:22:12.893Z

--- gulp/deploy.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-var ghPages = require('gulp-gh-pages');
-
-gulp.task('deploy', function() {
-  return gulp.src('./dist/**/*')
-      .pipe(ghPages());
-});
